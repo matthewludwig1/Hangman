36b3c01af1aff96a4ff88e6d86e80df31034a2ad
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-03-05 08:20:19 -0500

    Update Hangman.java

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,14 +1,17 @@
 /*
- 
+    Matthew Ludwig
+    5 March 2019
+    This program compares the users guess to a radomly selected phrase in various
+    categories
  */
 
-package hangman;
+package hangman2;
 
 /**
  *
  * @author malud0519
  */
-public class Hangman {
+public class Hangman2 {
 
     /**
      * @param args the command line arguments
@@ -16,8 +19,11 @@ public class Hangman {
     public static void main(String[] args) {
        String[] shows = {"The Walking Dead", "Family Guy", "American Dad", "Family"
                + "Feud"};
-       String[] videogames = {"Half Life 2", "Minecraft","Super Smash Bros"
-       , "Pokemon"};
+       String[] videoGames = {"Half Life", "Minecraft","Super Smash Bros", "Pokemon"};
+       String[] sportsTeams = {"Maple Leafs", "Yankees", "Canadiens" , "Raptors"};
+       String[] animals = {"Dog", "Cat", "Horse", "Moose"};
+       String[] foods = {"Pizza", "Yogurt", "Apple", "Lasagna"};
+       String[][] categories = {shows, videoGames, sportsTeams, animals, foods};
          
     }
     
