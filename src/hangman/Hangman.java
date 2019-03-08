1720c3e49f79bab46588dbefa3052b6611ae2d53
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-03-07 19:57:42 -0500

    Update Hangman.java

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,11 +1,11 @@
 /*
     Matthew Ludwig
-    5 March 2019
+    7 March 2019
     This program compares the users guess to a radomly selected phrase in various
     categories
  */
 
-package hangman;
+package hangman2;
 
 /**
  *
@@ -13,12 +13,14 @@ package hangman;
  */
 
 import javax.swing.*;
-public class Hangman {
+public class Hangman2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       char guess;
+       String input;
        String[] shows = {"The Walking Dead", "Family Guy", "American Dad", "Family"
                + "Feud"};
        String[] videoGames = {"Half Life", "Minecraft","Super Smash Bros", "Pokemon"};
@@ -26,17 +28,27 @@ public class Hangman {
        String[] animals = {"Dog", "Cat", "Horse", "Moose"};
        String[] foods = {"Pizza", "Yogurt", "Apple", "Lasagna"};
        String[][] categories = {shows, videoGames, sportsTeams, animals, foods};
+       int randomWord = (int)Math.random()* categories.length -1;
+       System.out.println("Category: ");
+       System.out.println("Number of Words: ");
        
-        for (int numGuesses = 7; numGuesses > 0; numGuesses--) {
-        
-            String guess = JOptionPane.showInputDialog("Number of remaining guesses: " +(numGuesses)
-        + " \n Guess a Letter:");
-        } 
+       for (int i = 0; i < categories.length; i++) { // display hyphens (does not work)
+           for (int j = 0; j < categories[i].length; i++ ) {
+               if (categories[i][j].charAt(i)== ' ') {
+                   System.out.println(" ");
+               }
+               else {
+                   System.out.println("-"); 
+               }
+                   
+           }
+       }
+       
+      
     }   
 }
 /*
 TO DO:
-Replace spaces with hyphens
 if the character is guessed correctly, show the letter on the screen
 else if the character is guessed incorrectly, display another part of the body.
     Also decrement the number of guesses remaining
@@ -46,4 +58,4 @@ else if the character is guessed incorrectly, display another part of the body.
 /*
 Displaying dashes for the string arrays:
 use if(charAt(i) == '') function
-*
+*/
